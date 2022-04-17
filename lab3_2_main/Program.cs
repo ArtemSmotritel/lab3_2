843f00c57e0a21feba455e1440b1fc911689fe42
ArtemSmotritel <artemsmotritel@gmail.com>
2022-04-17 17:22:38 +0300

    first commit

---
+++ lab3_2_main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab3_2_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First commit");
+        }
+    }
+}
