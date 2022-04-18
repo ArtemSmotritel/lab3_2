6c9ec7984cf970cbf89baeb57d331e05e510e902
veta306 <vv097099@gmail.com>
2022-04-18 17:10:42 +0300

    var12 method

--- lab3_2_main/var12_Vitaliy.cs
+++ lab3_2_main/var12_Vitaliy.cs
@@ -1,11 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace lab3_2_main
 {
     class Var12
     {
-        public string s = "12";
+        static void DoVar12(Student[] studs)
+        {
+            double avgScholarship = 0;
+            int n = 0;
+            for (int i = 0; i <= studs.Length; i++)
+            {
+                if (studs[i].scholarship != 0)
+                {
+                    avgScholarship += studs[i].scholarship;
+                    n++;
+                }
+            }
+            if (n == 0) Console.WriteLine("Таких студентів немає");
+            else
+            {
+                avgScholarship /= n;
+                Console.WriteLine("Середня стипендія " + avgScholarship);
+                Console.WriteLine("Студенти стипендія яких більш ніж на 20% менша середньої");
+                for (int i = 0; i <= studs.Length; i++)
+                {
+                    if (studs[i].scholarship != 0 && studs[i].scholarship < avgScholarship * 0.8)
+                    {
+                        Console.WriteLine(studs[i].surName + " " + studs[i].firstName + " " + studs[i].patronymic);
+                    }
+                }
+            }
+        }
     }
 }
